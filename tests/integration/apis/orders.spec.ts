5927490a6ca8b68ee7acf214265814e4c47e7a2e
Josh Fisher <jfisher@whitebox.com>
2021-09-16 17:58:55 -0400

    test: adding level 1 solution-provider-use-cases integration tests for orders

--- tests/integration/apis/orders.spec.ts
+++ tests/integration/apis/orders.spec.ts
@@ -1,8 +1,32 @@
 import * as env from '../../environment';
 import walmartApi, { OrdersApi, defaultParams } from '../../../src/index';
 import { addInterceptor, removeInterceptor } from '../../../utils/payload-interceptors';
+import { InlineResponse200ListElementsOrderLinesOrderLineStatusesOrderLineStatusStatusEnum } from '../../../src/apis/orders';
 
-describe('orders', () => {
+/**
+ * Convert order orderLines to shipping order order lines (because of course they are slightly different)
+ *
+ * @param multiLineOrders
+ * @returns
+ */
+const convertOrderLines = (multiLineOrders) => {
+	return multiLineOrders[0].orderLines.orderLine.map((orderLine) => {
+		return {
+			lineNumber: orderLine.lineNumber,
+			orderLineStatuses: {
+				orderLineStatus: orderLine.orderLineStatuses.orderLineStatus.map((orderLineStatus) => {
+					const duplicateOrderLineStatus = { ...orderLineStatus };
+					duplicateOrderLineStatus.status =
+						InlineResponse200ListElementsOrderLinesOrderLineStatusesOrderLineStatusStatusEnum.Shipped;
+					delete duplicateOrderLineStatus.cancellationReason;
+					return duplicateOrderLineStatus;
+				}),
+			},
+		};
+	});
+};
+
+describe(`${OrdersApi.name}`, () => {
 	let interceptor: any;
 	let ordersApi: OrdersApi;
 
@@ -37,7 +61,19 @@ describe('orders', () => {
 	});
 
 	describe('#getAllOrders', () => {
-		// TODO: Implement ME!!
+		it('should return a listing of orders', async () => {
+			const ordersResponse = await ordersApi.getAllOrders({
+				...defaultParams,
+				limit: '100',
+			});
+
+			expect(ordersResponse).toBeDefined();
+			expect(ordersResponse.data).toBeDefined();
+			expect(ordersResponse.data.list.errors).toBeUndefined();
+			expect(ordersResponse.data.list.elements).toBeDefined();
+			expect(ordersResponse.data.list.elements.order).toBeDefined();
+			expect(ordersResponse.data.list.elements.order).toBeInstanceOf(Array);
+		});
 	});
 
 	describe('#getAllReleasedOrders', () => {
@@ -45,7 +81,25 @@ describe('orders', () => {
 	});
 
 	describe('#getAnOrder', () => {
-		// TODO: Implement ME!!
+		it('should retrieve a single order', async () => {
+			const ordersResponse = await ordersApi.getAllOrders({
+				...defaultParams,
+				limit: '1',
+			});
+
+			const order = ordersResponse.data.list.elements.order[0];
+
+			const orderResponse = (await ordersApi.getAnOrder({
+				...defaultParams,
+				purchaseOrderId: order.purchaseOrderId,
+			})) as any;
+
+			expect(orderResponse).toBeDefined();
+			expect(orderResponse).toBeDefined();
+			expect(orderResponse.data).toBeDefined();
+			expect(orderResponse.data.order).toBeDefined();
+			expect(orderResponse.data.order.purchaseOrderId).toEqual(order.purchaseOrderId);
+		});
 	});
 
 	describe('#refundOrderLines', () => {
@@ -58,24 +112,102 @@ describe('orders', () => {
 
 	describe('solution-provider-use-cases', () => {
 		// Scenario 1 - Acknowledge - Acknowledge an order
-		it('should pass Scenario 1', () => {
-			// TODO: Implement ME!!
+		it('should pass Scenario 1', async () => {
+			// get an already Acknowledged order
+			const ordersResponse = await ordersApi.getAllOrders({
+				...defaultParams,
+				limit: '1',
+				status: 'Acknowledged',
+			});
 
-			expect(true).toEqual(false);
+			const order = ordersResponse.data.list.elements.order[0];
+
+			// re acknowledge an order so we comply with request but do not mess up any other orders
+			const acknowledgeOrderResponse = await ordersApi.acknowledgeOrders({
+				...defaultParams,
+				purchaseOrderId: order.purchaseOrderId,
+			});
+
+			expect(acknowledgeOrderResponse).toBeDefined();
+			expect(acknowledgeOrderResponse.data).toBeDefined();
+			expect(acknowledgeOrderResponse.data.order).toBeDefined();
+			expect(acknowledgeOrderResponse.data.order.purchaseOrderId).toBeDefined();
+			expect(acknowledgeOrderResponse.data.order.purchaseOrderId).toEqual(order.purchaseOrderId);
 		});
 
 		// Scenario 4 - Ship entire order - Ship an order with multi lines
-		it('should pass Scenario 4', () => {
-			// TODO: Implement ME!!
+		it('should pass Scenario 4', async () => {
+			// get an already shipped order
+			const ordersResponse = await ordersApi.getAllOrders({
+				...defaultParams,
+				limit: '100',
+				status: 'Delivered',
+				shipNodeType: 'SellerFulfilled',
+			});
 
-			expect(true).toEqual(false);
+			// find orders with multi lines
+			const multiLineOrders = ordersResponse.data.list.elements.order.filter((order) => {
+				return order.orderLines?.orderLine?.length > 1;
+			});
+
+			// convert order orderLines to shipping order order lines (because of course they are slightly different)
+			const shippingOrderOrderLines = convertOrderLines(multiLineOrders);
+
+			// response should throw 400 because this order was already delivered
+			const shipOrderResponse = await ordersApi.shippingUpdates({
+				...defaultParams,
+				purchaseOrderId: multiLineOrders[0].purchaseOrderId,
+				inlineObject: {
+					orderShipment: {
+						orderLines: {
+							orderLine: shippingOrderOrderLines as any,
+						},
+					},
+				},
+			});
+
+			expect(shipOrderResponse).toBeDefined();
+			expect(shipOrderResponse.data).toBeDefined();
+			expect(shipOrderResponse.data.order).toBeDefined();
+			expect(shipOrderResponse.data.order.purchaseOrderId).toBeDefined();
+			expect(shipOrderResponse.data.order.purchaseOrderId).toEqual(multiLineOrders[0].purchaseOrderId);
 		});
 
 		// Scenario 5 - Ship order line - Ship one order line at a time in a multi line order
-		it('should pass Scenario 5', () => {
-			// TODO: Implement ME!!
+		it('should pass Scenario 5', async () => {
+			// get an already shipped order
+			const ordersResponse = await ordersApi.getAllOrders({
+				...defaultParams,
+				limit: '100',
+				status: 'Delivered',
+				shipNodeType: 'SellerFulfilled',
+			});
 
-			expect(true).toEqual(false);
+			// find orders with multi lines
+			const multiLineOrders = ordersResponse.data.list.elements.order.filter((order) => {
+				return order.orderLines?.orderLine?.length > 1;
+			});
+
+			const shippingOrderOrderLines = convertOrderLines(multiLineOrders);
+
+			// response should throw 400 because this order was already delivered
+			const shipOrderResponse = await ordersApi.shippingUpdates({
+				...defaultParams,
+				purchaseOrderId: multiLineOrders[0].purchaseOrderId,
+				inlineObject: {
+					orderShipment: {
+						orderLines: {
+							orderLine: [shippingOrderOrderLines[0]] as any,
+						},
+					},
+				},
+			});
+
+			expect(shipOrderResponse).toBeDefined();
+			expect(shipOrderResponse.data).toBeDefined();
+			expect(shipOrderResponse.data.order).toBeDefined();
+			expect(shipOrderResponse.data.order.purchaseOrderId).toBeDefined();
+			expect(shipOrderResponse.data.order.purchaseOrderId).toEqual(multiLineOrders[0].purchaseOrderId);
 		});
 	});
 });
