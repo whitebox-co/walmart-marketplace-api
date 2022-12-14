7e28658593f237e7fa6ae845b6fa44bdc161b977
Josh Fisher <jpf200124@gmail.com>
2022-12-13 23:32:41 -0500

    chore: fix jest.config.js to handle new 1.x version of axios

--- jest.config.js
+++ jest.config.js
@@ -79,7 +79,9 @@ module.exports = {
 	// ],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+	moduleNameMapper: {
+		axios: 'axios/dist/node/axios.cjs',
+	},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
@@ -168,9 +170,7 @@ module.exports = {
 	// transform: null,
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-	// transformIgnorePatterns: [
-	//   "/node_modules/"
-	// ],
+	transformIgnorePatterns: ['node_modules/(?!axios)'],
 
 	// An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
 	// unmockedModulePathPatterns: undefined,
